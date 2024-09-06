d6340a973d43696717d3c52bc85e440217964cbf
ryotahoshino <75008198+ryotahoshino@users.noreply.github.com>
2024-09-06 14:03:38 +0900

    Storybookの導入

---
+++ .storybook/main.ts
@@ -0,0 +1,39 @@
+import type { StorybookConfig } from "@storybook/nextjs";
+import path from "path";
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-onboarding",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/nextjs",
+    options: {},
+  },
+  staticDirs: ["../public"],
+  babel: async option => ({
+    ...option,
+    plugin: [
+      '@babel/plugin-proposal-class-properties',
+      '@babel/plugin-proposal-private-methods',
+      '@babel/plugin-proposal-private-property-in-object'
+    ]
+  }),
+  webpackFinal: async (config) => {
+    if (config.resolve) {
+      config.resolve.plugins = [
+        ...(config.resolve.plugins || []),
+        new TsconfigPathsPlugin({
+          configFile: path.resolve(__dirname, '../tsconfig.json'),
+        }),
+      ];
+    }
+    return config;
+  },
+};
+export default config;
